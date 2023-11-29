954624f19f40bce8929dcf5d2c3cc3a7aa875650
Minji Kim <110662771+sloane323@users.noreply.github.com>
2023-11-29 06:29:56 +0000

    new

---
+++ new/src/App.js
@@ -0,0 +1,14 @@
+import Piechart from './Chart/Pie';
+import Side from './Side';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Side />
+    <Piechart />
+    </div>
+  );
+}
+
+export default App;
---
+++ new/src/Side.jsx
@@ -0,0 +1,26 @@
+import logo from "./Logo.svg";
+import './App.css';
+
+function Side() {
+  return (
+<div>
+
+<div className="sideall">
+  <img src={logo} alt="" />
+  <div> 목차 </div>
+  <div> 1 </div>
+  <div> 2 </div>
+  <div> 3 </div>
+  <div> 4 </div>
+  <div> 5 </div>
+  <div> 6 </div>
+</div>
+
+
+
+
+</div>
+  );
+}
+
+export default Side;
